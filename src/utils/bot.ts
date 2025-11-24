662d2c940ca7b730d09db7f09a9f709dac73ab40
ghxartya <artemijgorbulin@gmail.com>
2025-11-25 00:34:26 +0200

    Hotfix launch bot without webhook

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -3,13 +3,7 @@ import type { Bot } from '@/types'
 export async function launch(bot: Bot) {
   if (process.env.ENV_NAME === 'production') {
     console.log('Running bot in production mode (webhook)...')
-    await bot.launch({
-      webhook: {
-        domain: process.env.DOMAIN!,
-        port: Number(process.env.PORT!),
-        secretToken: bot.secretPathComponent()
-      }
-    })
+    await bot.launch()
   } else {
     console.log('Running bot in development mode (polling)...')
     void bot.launch()
